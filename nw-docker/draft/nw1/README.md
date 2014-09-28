730ff2ea51cbc0960a38f52d4c7e13a80ca37062
y12studio <y12studio@gmail.com>
2014-09-28 12:07:41 +0800

    tryrun1 : remote run y12docker/g0v-ivod

---
+++ nw-docker/draft/nw1/README.md
@@ -0,0 +1,7 @@
+﻿Test log
+
+```
+npm install
+node app.js
+curl http://192.168.59.103:9680/
+```
---
+++ nw-docker/draft/nw1/app.js
@@ -0,0 +1,47 @@
+﻿var Docker = require('dockerode');
+var dk = new Docker({
+		host : '192.168.59.103',
+		port : 2375
+	});
+
+function listContainers() {
+	dk.listContainers({
+		all : true
+	}, function (err, containers) {
+		console.log('ALL: ' + containers.length);
+		containers.forEach(function (c) {
+			console.log(c);
+		});
+	});
+}
+
+function runContainer() {
+	copt = {
+		"Image" : 'y12docker/g0v-ivod'
+	}
+	dk.createContainer(copt, function (err, container) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(container);
+			// [Docker Remote API v1.13 - Docker Documentation](https://docs.docker.com/reference/api/docker_remote_api_v1.13/#create-a-container)
+			sopt = {
+				"PortBindings" : {
+					"9680/tcp" : [{
+							"HostIp" : "0.0.0.0",
+							"HostPort" : "9680"
+						}
+					]
+				}
+			}
+
+			container.start(sopt, function (err, data) {
+				console.log(err == null ? data : err);
+			});
+
+		}
+
+	});
+}
+runContainer();
+listContainers();
