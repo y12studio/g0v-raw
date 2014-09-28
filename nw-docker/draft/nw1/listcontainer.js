f2e2afceb7e3b804aed48669d49fd9fdd391d100
y12studio <y12studio@gmail.com>
2014-09-28 17:59:16 +0800

    nw-docker list containers

---
+++ nw-docker/draft/nw1/listcontainer.js
@@ -0,0 +1,37 @@
+﻿var Docker = require('dockerode');
+var dk = new Docker({
+		host : '192.168.59.103',
+		port : 2375
+	});
+
+exports.list = function (cb) {
+	dk.listContainers({
+		all : true
+	}, cb);
+}
+
+listx = function () {
+	dk.listContainers({
+		all : true
+	}, function (err, containers) {
+		console.log('ALL: ' + containers.length);
+		containers.forEach(function (c) {
+			console.log(c);
+		});
+	});
+}
+
+listz = function (data) {
+    this.data = data;
+    this.callback = function (err, containers) {
+		console.log('ALL: ' + containers.length);
+		containers.forEach(function (c) {
+			console.log(c);
+            this.data.push(c);
+		});
+	}
+    
+	dk.listContainers({
+		all : true
+	}, this.callback);
+}
